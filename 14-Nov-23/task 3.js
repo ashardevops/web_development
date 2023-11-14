85f8182991bb1e1e4a4cd0e97827927ab99c1d9f
ashardevops <ahsan4280058@gmail.com>
2023-11-14 10:28:24 +0500

    this is quiz task

---
+++ 14-Nov-23/task 3.js
@@ -0,0 +1,14 @@
+for (var i=1; i<11 ; i++ ){
+    if (i==1){
+        console.log("gold medal")
+    }
+    else if (i==2){
+        console.log("silver medal")
+        }
+    else if (i==3) {
+        console.log("bronze medal")
+    }
+    else{
+        console.log(i);
+    }
+}
