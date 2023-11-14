3afa0b557cc44d48d71e2d18b158e5b13d7cd58d
ashardevops <ahsan4280058@gmail.com>
2023-11-14 09:46:35 +0500

    this is quiz 2

---
+++ 14-Nov-23/quiz2.js
@@ -0,0 +1,26 @@
+var day = "sunday";
+switch(day){
+    case 'monday':
+        console.log("Do somethig monday");
+        break;
+    case 'tuesday':
+        console.log("Do somethig tuesday");
+        break;
+    case 'wednesday':
+        console.log("Do somethig wednesday");
+        break;
+    case 'thursday':
+        console.log("Do somethig thursday");
+        break;
+    case 'friday':
+        console.log("Do somethig friday");
+        break;
+    case 'saturday':
+        console.log("Do somethig saturday");
+        break;
+    case 'sunday':
+        console.log("Do somethig sunday");
+        break;
+    default:
+        console.log('there is no such day')
+    } 
