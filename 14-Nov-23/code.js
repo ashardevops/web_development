110348c7aab364ee642686e7c78dcf0173792988
ashardevops <ahsan4280058@gmail.com>
2023-11-14 09:28:23 +0500

    this is assigument work

---
+++ 14-Nov-23/code.js
@@ -0,0 +1,15 @@
+var age=10;
+if (age>=65){
+console.log("you get your income from your passion.");
+
+}
+else if(age<65 && age>=18){
+    console.log("each mouth you get salary");
+
+}
+else if(age<18){
+    console.log("you get an allowance");
+}
+else{
+    console.log("the value of the variable is not numerical.");
+}
