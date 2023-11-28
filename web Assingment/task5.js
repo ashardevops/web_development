2f104f84d3935546ff4ac639d50291463e826074
ashardevops <ahsan4280058@gmail.com>
2023-11-28 10:08:16 +0500

    task5 web assing

---
+++ web Assingment/task5.js
@@ -0,0 +1,10 @@
+// Assuming some code logic before reaching this point with a loop
+// Let's say `i` is the index you're iterating through
+
+if (/* some condition */) {
+    // Code block if the condition is met
+    // ...
+  } else {
+    console.log('---No match found at', i);
+  }
+  
