258bf3c2993099ac45c913847a05a202b37a1a06
ashardevops <ahsan4280058@gmail.com>
2023-11-28 10:03:55 +0500

    this is task3 web asing

--- web Assingment/task3.js
+++ web Assingment/task3.js
@@ -1,6 +1,6 @@
 def letterFinder(word, match_letter):
     found_indices = []
     for i in range(len(word)):
-        if word[i] == match_letter:
+        if word[i]== match_letter;
             found_indices.append(i)
     return found_indices
