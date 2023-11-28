49122dd553ff6f3af6b061d905b74909b856637d
ashardevops <ahsan4280058@gmail.com>
2023-11-28 10:07:49 +0500

    update task1

--- web Assingment/task1.js
+++ web Assingment/task1.js
@@ -1,18 +1,13 @@
-function letterFinder(word, match) {
-    let indices = [];
-    
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === match) {
-        indices.push(i);
-      }
-    }
-    
-    return indices;
-  }
-  
-  // Example usage:
-  let word = "hello";
-  let match = "l";
-  let result = letterFinder(word, match);
-  console.log(`Indices of '${match}' in '${word}':`, result); // Output: Indices of 'l' in 'hello': [2, 3]
-  
+def letterFinder(word, match):
+    """
+    Function to find the positions of a specific letter (match) in a given word.
+
+    Parameters:
+    word (str): The word in which to search for the specified letter.
+    match (str): The letter to find in the word.
+
+    Returns:
+    list: A list containing the positions (indices) where the letter is found in the word.
+    """
+    positions = [index for index, letter in enumerate(word) if letter == match]
+    return positions
